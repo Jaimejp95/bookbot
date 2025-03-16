029d58182f49bf10a60a5fa744569ebaaba747ec
Jamille <jaimejp95@live.com>
2025-03-16 09:48:15 -0400

    Updated pull method 2 new books

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import count_words, count_chars, sort_char_count
 
 def get_book_text(filepath):
@@ -5,15 +6,20 @@ def get_book_text(filepath):
         return f.read()
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    filepath = sys.argv[1]  # Get the book path from command-line arguments
+    text = get_book_text(filepath)
     num_words = count_words(text)
     print(f"{num_words} words found in the document")
     char_counts = count_chars(text)
     print(char_counts)
     sorted_chars = sort_char_count(char_counts)
-        # Print the report
+    # Print the report
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {"books/frankenstein.txt"}...")
+    print(f"Analyzing book found at {filepath}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
